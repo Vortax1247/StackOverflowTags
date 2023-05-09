1d72ed4ec90cc5c5c00f236221bbc4052448c529
Adrien HUMMEL <132281657+Vortax1247@users.noreply.github.com>
2023-05-09 16:02:42 +0200

    Update model_text.py

--- model_text.py
+++ model_text.py
@@ -8,6 +8,7 @@ from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
 
 df_study  = pd.read_csv("tags_learning.csv").drop('Unnamed: 0',axis=1)
 y  = pd.read_csv('tags_y_learning.csv').drop('Unnamed: 0',axis=1)
@@ -20,10 +21,10 @@ y = multilabel_binarizer.transform(df_study.Tags)
 
 X = text_preprocessing(df_study)
 model = Pipeline([('tfidf', TfidfVectorizer(max_df=0.5, min_df=5, stop_words="english", max_features = 1000)),
-          ('svc', LinearSVC())])
+          ('multi_svc', OneVsRestClassifier(LinearSVC()))])
 X_train,X_test,y_train,y_test = train_test_split(X, y, train_size= 0.60, random_state=9000)
 
-model.train(X_train,y_train)
+model.fit(X_train,y_train)
 y_pred  = model.predict(X_test)
 print_score(y_test,y_pred)
 
