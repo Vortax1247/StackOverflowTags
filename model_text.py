d52c8909d65964b6c5c74e2e7eb34c6c6ed0f558
Adrien HUMMEL <132281657+Vortax1247@users.noreply.github.com>
2023-05-09 15:43:01 +0200

    Update model_text.py
    Pickle pipe and multibinarizer

--- model_text.py
+++ model_text.py
@@ -3,33 +3,34 @@ from sklearn.preprocessing import MultiLabelBinarizer
 import pandas as pd
 import numpy as np
 from text_preprocess import *
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
-import time
-import logging
-
-logging.disable(logging.WARNING) # disable WARNING, INFO and DEBUG logging everywhere
-
-
-df_study     = pd.read_csv("tags_learning.csv").drop('Unnamed: 0',axis=1)
+df_study  = pd.read_csv("tags_learning.csv").drop('Unnamed: 0',axis=1)
 y  = pd.read_csv('tags_y_learning.csv').drop('Unnamed: 0',axis=1)
 
-from sklearn.preprocessing import MultiLabelBinarizer
+
 
 multilabel_binarizer = MultiLabelBinarizer()
-multilabel_binarizer.fit(data_T.Tags[0:-2])
-Y = multilabel_binarizer.transform(data_T.Tags[0:-2])
+multilabel_binarizer.fit(data_T.Tags)
+y = multilabel_binarizer.transform(data_T.Tags)
+
+X = text_preprocessing(df_study)
+model = Pipeline([('tfidf', TfidfVectorizer(max_df=0.5, min_df=5, stop_words="english", max_features = 1000)),
+          ('svc', LinearSVC())])
+X_train,X_test,y_train,y_test = train_test_split(X, y, train_size= 0.60, random_state=9000)
+
+model.train(X_train,y_train)
+y_pred  = model.predict(X_test)
+print_score(y_test,y_pred)
+
 pickle_out = open('binarizer.pkl', "wb")
 pickle.dump(multilabel_binarizer, pickle_out)
 pickle_out.close()
-df_study['Text_Title'] = df_study['Title_bow_lem'].apply(transform_bow_lem_spacy_fct)+' '+df_study['Text_bow_lem'].apply(transform_text_bow_lem_spacy_fct)
 
-batch_size = 10
-sentences = df_study['Text_Title'].to_list()
-
-features_USE = feature_USE_fct(sentences, batch_size)
-
-
-
-model =  pickle.load("model_text.pkl",rb)
-y_pred = model.predict(features_USE)
-print_score(y,y_pred)
+pickle_out = open('model_text.pkl', "wb")
+pickle.dump(model, pickle_out)
+pickle_out.close()
