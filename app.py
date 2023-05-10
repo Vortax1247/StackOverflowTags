058256fe18ed2a19ba2d70488b4fe46c45841f31
Adrien HUMMEL <132281657+Vortax1247@users.noreply.github.com>
2023-05-10 12:41:45 +0200

    Update app.py
    Request works now

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
+from text_preprocess import text_preprocessing
 
 app = Flask(__name__)
 model = pickle.load(open('model_text.pkl', 'rb'))
-
+multibinazier = pickle.load(open('binarizer.pkl','rb'))
 
    
 @app.route('/results',methods=['POST'])
 def results():
-
-    data = request.get_json(force=True)
-    prediction = model_text.predict(text_processing(request))
-    tags = multibinazieer.inverse_transform(prediction)
+    data = pd.DataFrame(request.get_json())
+    prediction = model.predict(text_preprocessing(data))
+    tags = multibinazier.inverse_transform(prediction)
     return jsonify(tags)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
