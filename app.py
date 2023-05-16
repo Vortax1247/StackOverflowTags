cc387a4a1e5af283ff538d1d5703a744c585d324
Adrien HUMMEL <132281657+Vortax1247@users.noreply.github.com>
2023-05-16 11:13:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 from text_preprocess import text_preprocessing
+import os
 
 app = Flask(__name__)
 model = pickle.load(open('model_text.pkl', 'rb'))
