b6a5104216fab9cb49c3445f23163e6370722258
Adrien HUMMEL <132281657+Vortax1247@users.noreply.github.com>
2023-05-26 17:58:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 from text_preprocess import text_preprocessing
-import os
 
 app = Flask(__name__)
 model = pickle.load(open('model_text.pkl', 'rb'))
@@ -20,5 +19,3 @@ def results():
     prediction = model.predict(text_preprocessing(data))
     tags = multibinazier.inverse_transform(prediction)
     return jsonify(tags)
-
-
